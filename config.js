fd80ddf7036934ea3db168a12f54e3e38635ad40
GiangQuynh <86777202+GiangQuynh@users.noreply.github.com>
2021-07-01 17:40:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Sau này cưới anh nha <33',
     btnNo: 'Hay chỉ iu a hoi :(( :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Hứa với anh nhé !! <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em hứa yêu anh tới già luôn, tới chết luôn >3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Đừng xa anh nha, nói rồi đấy.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    }
